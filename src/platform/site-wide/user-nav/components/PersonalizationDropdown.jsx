fd0069623ff31286210fff4ddfe3fff7dc543e53
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-09-27 15:31:44 -0400

    Revert "Delete all unused directories"
    This reverts commit 3107231db90f36bf13d5f082f3018ab399177f5e.

---
+++ src/platform/site-wide/user-nav/components/PersonalizationDropdown.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { logout } from '../../../user/authentication/utilities';
+import dashboardManifest from '../../../../applications/personalization/dashboard/manifest.json';
+import recordEvent from '../../../../platform/monitoring/record-event';
+import isBrandConsolidationEnabled from '../../../../platform/brand-consolidation/feature-flag';
+
+const LEFT_CLICK = 1;
+const dashboardLink = dashboardManifest.rootUrl;
+const brandConsolidationEnabled = isBrandConsolidationEnabled();
+
+function NewBadge() {
+  return <span className="usa-label va-label-primary">New</span>;
+}
+
+class PersonalizationDropdown extends React.Component {
+  componentDidMount() {
+    // remove checkLink function when refactoring out isBrandConsolidationEnabled
+    if (!brandConsolidationEnabled) { document.addEventListener('click', this.checkLink); }
+  }
+
+  componentWillUnmount() {
+    if (!brandConsolidationEnabled) { document.removeEventListener('click', this.checkLink); }
+  }
+
+  checkLink = (event) => {
+    const target = event.target;
+    if (
+      target.tagName.toLowerCase() === 'a' &&
+      target.hostname === document.location.hostname &&
+      target.pathname === '/' &&
+      event.which === LEFT_CLICK) {
+      target.href = dashboardLink;
+    }
+  }
+
+  render() {
+    return (
+      <ul>
+        {brandConsolidationEnabled && <li><a href="/dashboard/">My VA</a></li>}
+        {brandConsolidationEnabled && <li><a href="https://www.myhealth.va.gov/mhv-portal-web/home">My Health</a></li>}
+        <li><a href="/profile" onClick={() => { recordEvent({ event: 'nav-user', 'nav-user-section': 'profile' });}}>Profile</a> <NewBadge/></li>
+        <li><a href="/account" onClick={() => { recordEvent({ event: 'nav-user', 'nav-user-section': 'account' });}}>Account</a> <NewBadge/></li>
+        <li><a href="#" onClick={logout}>Sign Out</a></li>
+      </ul>
+    );
+  }
+}
+
+export default PersonalizationDropdown;
