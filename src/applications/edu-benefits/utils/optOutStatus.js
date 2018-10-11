bfeb8ff598b35cf03e46e5c9e60d46cd1789fe1e
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-11 14:09:15 -0400

    Regenerating branch from vets-website va-gov/pages

--- src/applications/edu-benefits/utils/optOutStatus.js
+++
@@ -1,9 +0,0 @@
-import OptOutWizard from '../components/OptOutWizard';
-import ApplicationStatus from '../../../platform/forms/save-in-progress/ApplicationStatus';
-
-// helper module so that we can code split with both of these components
-// in the same bundle
-export default {
-  OptOutWizard,
-  ApplicationStatus,
-};
--- src/platform/utilities/constants.js
+++
@@ -1,7 +0,0 @@
-// TODO: Rename this to something that makes more sense
-export const requestStates = {
-  notCalled: 'not called',
-  pending: 'pending',
-  succeeded: 'succeeded',
-  failed: 'failed',
-};
