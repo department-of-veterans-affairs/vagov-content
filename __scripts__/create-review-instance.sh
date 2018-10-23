e3413b8142d3225cd5f8b51116f1d7fba7a12b79
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-23 18:46:52 -0400

    DO NOT CHECK BROKEN LINKS

--- __scripts__/create-review-instance.sh
+++ __scripts__/create-review-instance.sh
@@ -3,4 +3,4 @@ vagov_content_dir=${PWD}
 git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
 cd ../vagov-apps
 yarn install --production=false
-npm run build -- --entry static-pages,style --brand-consolidation-enabled --content-directory=${vagov_content_dir}
+CHECK_BROKEN_LINKS=no npm run build -- --entry static-pages,style --brand-consolidation-enabled --content-directory=${vagov_content_dir}
