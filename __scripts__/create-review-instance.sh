fda7a517537a288c164070bcb704ca9cd4c54756
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-23 16:54:35 -0400

    Fix sh script

--- __scripts__/create-review-instance.sh
+++ __scripts__/create-review-instance.sh
@@ -1,3 +1,4 @@
 git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
-cd ../vagov-apps && npm install
+cd ../vagov-apps
+yarn install
 npm run heroku-postbuild
