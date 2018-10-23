2374c94e695b2650fe66a8b9933303b75d5d3217
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-23 18:20:15 -0400

    Pass the content directory

--- __scripts__/create-review-instance.sh
+++ __scripts__/create-review-instance.sh
@@ -1,4 +1,5 @@
 git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
 cd ../vagov-apps
 yarn install --production=false
-npm run build -- --entry static-pages,style --brand-consolidation-enabled
+vagov_content_dir=${PWD##*/}
+npm run build -- --entry static-pages,style --brand-consolidation-enabled --content-directory=${vagov_content_dir}
