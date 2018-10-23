5e0cee574ab57f393c16d3110639597f2ac72192
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-23 18:42:48 -0400

    Again, don't check broken links

--- __scripts__/create-review-instance.sh
+++ __scripts__/create-review-instance.sh
@@ -1,3 +1,4 @@
+CHECK_BROKEN_LINKS=no
 vagov_content_dir=${PWD}
 git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
 cd ../vagov-apps
