7a005df4aea36398a487a457e5ca4baf0952ff65
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-23 18:25:23 -0400

    Move variable to the top

--- __scripts__/create-review-instance.sh
+++ __scripts__/create-review-instance.sh
@@ -1,5 +1,5 @@
+vagov_content_dir=${PWD##*/}
 git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
 cd ../vagov-apps
 yarn install --production=false
-vagov_content_dir=${PWD##*/}
 npm run build -- --entry static-pages,style --brand-consolidation-enabled --content-directory=${vagov_content_dir}
