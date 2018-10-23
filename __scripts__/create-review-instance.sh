c38a4cf3aee36ab88f7142ee29f6bd1299920a8a
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-23 17:48:35 -0400

    Add brand consolidation flag

--- __scripts__/create-review-instance.sh
+++ __scripts__/create-review-instance.sh
@@ -1,4 +1,4 @@
 git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
 cd ../vagov-apps
 yarn install --production=false
-npm run heroku-postbuild
+npm run build -- --entry static-pages,style --brand-consolidation-enabled
