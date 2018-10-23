be0052d9f6bd320ca60cc8c088d99c54533277d2
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-23 16:52:19 -0400

    Fix clone

--- __scripts__/create-review-instance.sh
+++ __scripts__/create-review-instance.sh
@@ -1,3 +1,3 @@
-git clone --single-branch interim-cms --depth 1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
+git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
 cd ../vagov-apps && npm install
 npm run heroku-postbuild
