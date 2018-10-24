e645e50e4d7180851332453bd44ce52b99eb86ad
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-24 13:28:56 -0400

    Update generate script

--- scripts/regenerate-pages.sh
+++ scripts/regenerate-pages.sh
@@ -4,7 +4,9 @@ echo pulling latest...
 git fetch vets-website
 
 echo grabbing the commit history
-git merge --strategy-option=theirs vets-website/master -m "Merging w/latest vets-website..."
+git merge --strategy-option=theirs vets-website/master
+git add .
+git commit -m "Taking latest from vets-website"
 
 echo removing stale files
 git rm -r .
