7871c95b8cf50a8326ac4e0494b0005110561e26
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-24 06:46:02 -0400

    MOre debug

--- __scripts__/heroku-postbuild.sh
+++ __scripts__/heroku-postbuild.sh
@@ -3,29 +3,34 @@ export CHECK_BROKEN_LINKS="no"
 vagov_content_dir=${PWD}
 vagov_apps_dir = ${vagov_content_dir}/.vagov-apps
 
+echo vagov-content located at ${vagov_content_dir}
+echo Installing vagov-apps into ${vagov_apps_dir}
+
 # Install into a subdirectory, so that we're safe from tmp storage.
 git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ${vagov_apps_dir}
 
-# CD into the newly-cloned repo
-cd ${vagov_apps_dir}
+# cd into the newly-cloned repo
+# cd ${vagov_apps_dir}
 
-# Install all dependencies, including devDependencies
-yarn install --production=false
+# # Install all dependencies, including devDependencies
+# yarn install --production=false
 
-# Execute the build script
-npm run build -- --entry static-pages,style --brand-consolidation-enabled --content-directory=${vagov_content_dir}
+# # Execute the build script
+# npm run build -- --entry static-pages,style --brand-consolidation-enabled --content-directory=${vagov_content_dir}
 
-ls
 
-cd ${vagov_apps_dir}/build
+# ls
 
-echo build contents
+# #
+# cd ${vagov_apps_dir}/build
 
-ls
+# PWD
 
-echo ${vagov_content_dir}/build
+# ls
 
-# Move the directory out of /tmp so the files persist
-mv ${vagov_apps_dir}/build ${vagov_content_dir}/build
+# echo ${vagov_content_dir}/build
+
+# # Move the directory out of /tmp so the files persist
+# mv ${vagov_apps_dir}/build ${vagov_content_dir}/build
 
 # asd
