6c41a0c8a7746d83111cf5ab8245d8bc0b8fe631
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-24 06:02:07 -0400

    Add debug code

--- __scripts__/create-review-instance.sh
+++ __scripts__/create-review-instance.sh
@@ -1,9 +1,20 @@
 export CHECK_BROKEN_LINKS="no"
 
+# Get the absolute path of the content directory
 vagov_content_dir=${PWD}
+
 git clone --branch interim-cms --depth=1 https://github.com/department-of-veterans-affairs/vets-website ../vagov-apps
 cd ../vagov-apps
 yarn install --production=false
+
+# Execute the build script
 npm run build -- --entry static-pages,style --brand-consolidation-enabled --content-directory=${vagov_content_dir}
 
+ls
+
+cd build
+
+ls
+
+# Move the directory out of /tmp so the files persist
 mv build/heroku ${vagov_content_dir}/__build__
