671975e06d9cf4a42d7d6b129414ae67af3c6e22
Nick Sullivan <nick.sullivan@adhocteam.us>
2018-10-29 12:32:11 -0400

    add branch config

--- scripts/heroku-postbuild.sh
+++ scripts/heroku-postbuild.sh
@@ -7,7 +7,7 @@ echo vagov-content located at ${vagov_content_dir}
 echo Installing vagov-apps into ${vagov_apps_dir}
 
 # Install into a subdirectory, so that we're safe from tmp storage.
-git clone --depth=1 https://github.com/department-of-veterans-affairs/vets-website ${vagov_apps_dir}
+git clone -b ns-14607 --depth=1 https://github.com/department-of-veterans-affairs/vets-website ${vagov_apps_dir}
 
 # cd into the newly-cloned repo
 cd ${vagov_apps_dir}
@@ -16,7 +16,7 @@ cd ${vagov_apps_dir}
 yarn install --production=false
 
 # # Execute the build script
-npm run build -- --entry static-pages --brand-consolidation-enabled --content-directory=${vagov_content_dir}/pages
+npm run build -- --entry static-pages --brand-consolidation-enabled --content-directory=${vagov_content_dir}/pages --content-deployment
 
 # Move into vagov_content so that the build folder is easily references by the Procfile
 mv ${vagov_apps_dir}/build ${vagov_content_dir}/build
